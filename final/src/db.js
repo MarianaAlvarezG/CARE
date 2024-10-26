8d71a94753f5e63fd72723a820132b3b7c19e607
MarianaAlvarezG <147668306+MarianaAlvarezG@users.noreply.github.com>
2024-10-26 11:03:40 -0400

    Update db.js

--- final/src/db.js
+++ final/src/db.js
@@ -1,14 +1,18 @@
 const mysql = require('mysql2/promise');
 
-// Create a connection pool using environment variables
+
+// Create a connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME, 
+  host: 'localhost', 
+  user: 'root', 
+  password: 'Pizzalover2003', 
+  database: 'care', 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
+
+
 module.exports = pool;
+

1efb77b26c0901447028f03fdfefe610a443407b
MarianaAlvarezG <147668306+MarianaAlvarezG@users.noreply.github.com>
2024-10-25 22:15:24 -0400

    Create components folder

---
+++ src/components/Account.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Account.css';
+
+const Account = () => {
+  const navigate = useNavigate();
+
+  const setUserType = async (type) => {
+    sessionStorage.setItem('userType', type);
+
+    try {
+      const response = await fetch('/auth/account', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userType: type }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('User type set successfully:', data);
+        navigate('/auth/login');
+      } else {
+        alert('Failed to set user type.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const redirectToRegister = () => {
+    navigate('/auth/register');
+  };
+
+  return (
+    <div className="main-container">
+      <img src="../views/images/CARE.png" alt="CARE Logo" className="care-logo" />
+      <span className="returning-user">Returning user?</span>
+      <button className="rectangle-button" onClick={() => setUserType('admin')}>
+        <span className="admin-span">Admin</span>
+      </button>
+      <button className="rectangle-button-2" onClick={() => setUserType('user')}>
+        <span className="user-span">User</span>
+      </button>
+      <div className="new-here-div">
+        <span className="text-5">New here?</span>
+        <span className="text-6" onClick={redirectToRegister}>
+          Create an account
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Account;

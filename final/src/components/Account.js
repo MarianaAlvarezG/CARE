f80e6351ea8770255f2b0a93c7321c269c216582
MarianaAlvarezG <147668306+MarianaAlvarezG@users.noreply.github.com>
2024-10-26 14:26:06 -0400

    Update Account.js path

--- final/src/components/Account.js
+++ final/src/components/Account.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Account.css';
-import careLogo from './views/images/CARE.png';
+import careLogo from '../views/images/CARE.png';
 
 
 const Account = () => {
